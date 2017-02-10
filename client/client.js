60ebf377619ea30b5a52c667a4a1a52582843768
J.W. Clark <jwclark@rockhursths.edu>
2017-02-10 11:07:13 -0600

    init

---
+++ client/client.js
@@ -0,0 +1,178 @@
+// TODO : http://stackoverflow.com/questions/38240504/refresh-expired-jwt-in-browser-when-using-google-sign-in-for-websites
+// OR   : http://stackoverflow.com/questions/32150845/how-to-refresh-expired-google-sign-in-logins?rq=1
+// READ : http://stackoverflow.com/questions/3105296/if-rest-applications-are-supposed-to-be-stateless-how-do-you-manage-sessions
+// READ : http://www.cloudidentity.com/blog/2014/03/03/principles-of-token-validation/
+
+// this statement is a redirect for brackets development
+if (window.location.hostname === '127.0.0.1') {
+    window.location = 'http://localhost:1898';
+}
+
+// prepend the url of node.js server
+function route(url) {
+    return 'http://localhost:3000' + url;
+}
+
+var profile; // google user profile
+var authResponse; // google user auth response
+
+function onSignIn(googleUser) {
+    profile = googleUser.getBasicProfile();
+    authResponse = googleUser.getAuthResponse();
+
+    var login = {
+        id: profile.getId(),
+        name: profile.getName(),
+        givenName: profile.getGivenName(),
+        familyName: profile.getFamilyName(),
+        imageUrl: profile.getImageUrl(),
+        email: profile.getEmail(),
+        hostedDomain: googleUser.getHostedDomain()
+    };
+
+    http.post('/login', login);
+    
+    http.get('/tasks', function(data, statusText, jqXHR) {
+        if(jqXHR.status === 200) {
+            data.forEach(function(obj) {
+                $('#todo-list').append('<div data-id="' + obj._id + '" contenteditable="true" class="dc well well-lg col-md-12">' + obj.text + '<span class="glyphicon glyphicon-trash"></span></div>');    
+            });
+        } else {
+            alert('no milk :(');
+        }
+    }, function(jqXHR, textStatus, errorThrown) {
+        alert('error');
+    });
+    
+    $('.g-signin2').hide();
+    $('#g-signout').show();
+    $('#app').show();
+}
+
+function signOut() {
+    gapi.auth2.getAuthInstance().signOut();
+    $('.g-signin2').show();
+    $('#g-signout').hide();
+    $('#app').hide();
+    $('#todo-list').html('');
+}
+
+function disconnect() {
+    gapi.auth2.getAuthInstance().disconnect();
+    $('.g-signin2').show();
+    $('#g-signout').hide();
+    $('#app').hide();
+    $('#todo-list').html('');
+}
+
+var http = {};
+
+http.post = function(url, json, success, error) {
+    $.ajax({
+        url: route(url),
+        method: 'POST',
+        data: json,
+        headers: {
+            'Authorization': authResponse.id_token
+        },
+        success: function(data, statusText, jqXHR) {
+            if (success) success(data, statusText, jqXHR);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if (error) error(jqXHR, textStatus, errorThrown);
+        }
+    });
+};
+
+http.get = function(url, success, error) {
+    $.ajax({
+        url: route(url),
+        method: 'GET',
+        headers: {
+            'Authorization': authResponse.id_token
+        },
+        success: function(data, statusText, jqXHR) {
+            if (success) success(data, statusText, jqXHR);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if (error) error(jqXHR, textStatus, errorThrown);
+        }
+    });
+};
+
+http.put = function(url, json, success, error) {
+    $.ajax({
+        url: route(url),
+        method: 'PUT',
+        data: json,
+        headers: {
+            'Authorization': authResponse.id_token
+        },
+        success: function(data, statusText, jqXHR) {
+            if (success) success(data, statusText, jqXHR);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if (error) error(jqXHR, textStatus, errorThrown);
+        }
+    });
+};
+
+http.delete = function(url, success, error) {
+    $.ajax({
+        url: route(url),
+        method: 'DELETE',
+        headers: {
+            'Authorization': authResponse.id_token
+        },
+        success: function(data, statusText, jqXHR) {
+            if (success) success(data, statusText, jqXHR);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            if (error) error(jqXHR, textStatus, errorThrown);
+        }
+    });
+};
+
+$('#new-task').keypress(function(e) {
+    if (e.which == 13) { // the enter key
+        e.preventDefault();
+        
+        var payload = { text: $(this).text() };
+        
+        http.post('/task', payload, function(data, statusText, jqXHR) {
+            if (jqXHR.status === 201) {
+                $('#todo-list').append('<div data-id="' + data.insertedIds[0] + '" contenteditable="true" class="dc well well-lg col-md-12">' + data.ops[0].text + '<span class="glyphicon glyphicon-trash"></span></div>');
+                $('#new-task').html('');
+            } else {
+                alert('error :S');
+            }
+        });
+    }
+});
+
+$('#app').on('keypress', '.dc', function(e) {
+    if (e.which == 13) { // the enter key
+        e.preventDefault();
+        
+        var taskElement = $(this);
+        var data = { text: taskElement.text() };
+        var taskId = taskElement.data('id');
+        
+        http.put('/task/' + taskId, data, function(data, statusText, jqXHR) {
+            if(jqXHR.status === 200) {
+                taskElement.blur();
+            }
+        });
+    }
+});
+
+$('#app').on('click', '.well .glyphicon-trash', function() {
+    var taskElement = $(this).parent();
+    var taskId = taskElement.data('id');
+    
+    http.delete('/task/' + taskId, function(data, statusText, jqXHR) {
+        if(jqXHR.status === 200) {
+            taskElement.remove();
+        }
+    });
+});
---
+++ node/server.js
@@ -0,0 +1,289 @@
+var fs = require('fs'); // file systems
+var jwt = require('jsonwebtoken'); // json web tokens
+var http = require('http'); // http protocol
+var express = require('express'); // web server
+var request = require('request'); // http trafficer
+var jwkToPem = require('jwk-to-pem'); // converts json web key to pem
+var bodyParser = require('body-parser'); // http body parser
+var mongodb = require('mongodb'); // MongoDB driver
+
+var Mongo = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
+
+var keyCache = {}; // public key cache
+
+const MONGO_URL = 'mongodb://localhost:27017/todo';
+const CLIENT_ID = '570807057244-dgdihgn2co89soo39sb6td3o2j0m6f5p.apps.googleusercontent.com';
+
+/**
+ * MongoDB operations
+ * connects to MongoDB and registers a series of asynchronous methods
+ */
+Mongo.connect(MONGO_URL, function(err, db) {
+    
+    // TODO: handle err
+
+    Mongo.ops = {};
+    
+    Mongo.ops.find = function(collection, json, callback) {
+        db.collection(collection).find(json).toArray(function(error, docs) {
+            // TODO: handle err
+            if(callback) callback(error, docs);
+        });
+    };
+    
+    Mongo.ops.findOne = function(collection, json, callback) {
+        db.collection(collection).findOne(json, function(error, doc) {
+            // TODO: handle err
+            if(callback) callback(error, doc);
+        });
+    };
+
+    Mongo.ops.insert = function(collection, json, callback) {
+        db.collection(collection).insert(json, function(error, result) {
+            // TODO: handle err
+            if(callback) callback(error, result);
+        });
+    };
+
+    Mongo.ops.upsert = function(collection, query, json, callback) {
+        db.collection(collection).updateOne(query, { $set: json }, { upsert: true }, function(error, result) {
+            // TODO: handle err
+            if (callback) callback(error, result);
+        });
+    };
+    
+    Mongo.ops.updateOne = function(collection, query, json, callback) {
+        db.collection(collection).updateOne(query, { $set : json }, function(error, result) {
+            // TODO: handle err
+            if(callback) callback(error, result);
+        });
+    };
+    
+    Mongo.ops.deleteOne = function(collection, query, callback) {
+        db.collection(collection).deleteOne(query, function(error, result) {
+            // TODO: handle err
+            if(callback) callback(error, result);
+        });
+    };
+    
+    Mongo.ops.deleteMany = function(collection, query, callback) {
+        db.collection(collection).deleteMany(query, function(error, result) {
+            // TODO: handle err
+            if(callback) callback(error, result);
+        });
+    };
+});
+
+
+
+// web server
+var app = express();
+
+// use middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(allowCrossDomain);
+app.use(authorize);
+
+app.post('/login', function(req, res) {
+    log('/login req.body = ', req.body);
+    var query = { id: req.body.id };
+    Mongo.ops.upsert('login', query, req.body);
+    res.status(201).send('ok');
+});
+
+app.post('/task', function(req, res) {
+    Mongo.ops.insert('task', payload(req), function(error, result) {
+        log('post /task = ', payload(req));
+        if(error) res.status(500).send(error);
+        else res.status(201).send(result);
+    });
+});
+
+app.put('/task/:taskId', function(req, res) {
+    var query = { _id : new ObjectID(req.params.taskId) };
+    query.userid = getUserId(req);
+    Mongo.ops.updateOne('task', query, req.body, function(error, result) {
+        log('put /task/:taskId = ', query);
+        if(error) res.status(500).send(error);
+        else res.status(200).send(result);
+    });
+});
+
+app.delete('/task/:taskId', function(req, res) {
+    var query = payload(req);
+    query._id = new ObjectID(req.params.taskId);
+    Mongo.ops.deleteOne('task', query, function(error, result) {
+        log('delete /task/:taskId = ', query);
+        if(error) res.status(500).send(error);
+        else res.status(200).send(result);
+    });
+});
+
+app.get('/tasks', function(req, res) {
+    Mongo.ops.find('task', payload(req), function(error, docs) {
+        log('get /tasks = ', payload(req));
+        if(error) res.status(500).send(error);
+        else res.status(200).send(docs);
+    });
+});
+
+// listen on port 3000
+app.listen(3000, function() {
+    cacheWellKnownKeys();
+    log('listening on port 3000');
+});
+
+
+
+/**
+ * Middleware:
+ * allows cross domain requests
+ * ends preflight checks
+ */
+function allowCrossDomain(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization');
+
+    // end pre flights
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+    } else {
+        next();
+    }
+}
+
+/**
+ * Middlware:
+ * validate tokens and authorize users
+ */
+function authorize(req, res, next) {
+
+    // jwt.decode: https://github.com/auth0/node-jsonwebtoken#jwtdecodetoken--options
+    // jwt.verify: https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+
+    try {
+        var token       = req.headers.authorization;
+        var decoded     = jwt.decode(token, { complete: true });
+        var keyID       = decoded.header.kid;
+        var algorithm   = decoded.header.alg;
+        var iss         = decoded.payload.iss;
+        var pem         = getPem(keyID);
+
+        if (iss === 'accounts.google.com' || iss === 'https://accounts.google.com') {
+            var options = {
+                audience: CLIENT_ID,
+                issuer: iss,
+                algorithms: [algorithm]
+            }
+
+            jwt.verify(token, pem, options, function(err) {
+                if (err) {
+                    res.writeHead(401);
+                    res.end();
+                } else {
+                    next();
+                }
+            });            
+
+        } else {
+            res.writeHead(401);
+            res.end();
+        }
+    } catch (err) {
+        res.writeHead(401);
+        res.end();
+    }
+}
+
+/**
+ * Attach user ID to their payload
+ */
+function payload(req) {
+    if(req.body) {
+        var data = req.body;
+        data.userid = getUserId(req);
+        return data;        
+    } else {
+        return { userid : getUserId(req) };
+    }
+}
+
+/**
+ * Get userid from idtoken in authorization header
+ */
+function getUserId(req) {
+    var idToken = req.headers.authorization;
+    return jwt.decode(idToken).sub;
+};
+
+/**
+ * Converts json web key to pem key
+ */
+function getPem(keyID) {
+    var jsonWebKeys = keyCache.keys.filter(function(key) {
+        return key.kid === keyID;
+    });
+    return jwkToPem(jsonWebKeys[0]);
+}
+
+/**
+ * Cache Google's well known public keys
+ */
+function cacheWellKnownKeys() {
+
+    // get the well known config from google
+    request('https://accounts.google.com/.well-known/openid-configuration', function(err, res, body) {
+        var config = JSON.parse(body);
+        var address = config.jwks_uri; // ex: https://www.googleapis.com/oauth2/v3/certs
+
+        // get the public json web keys
+        request(address, function(err, res, body) {
+
+            keyCache.keys = JSON.parse(body).keys;
+
+            // example cache-control header: 
+            // public, max-age=24497, must-revalidate, no-transform
+            var cacheControl = res.headers['cache-control'];
+            var values = cacheControl.split(',');
+            var maxAge = parseInt(values[1].split('=')[1]);
+
+            // update the key cache when the max age expires
+            setTimeout(cacheWellKnownKeys, maxAge * 1000);
+
+            log('Cached keys = ', keyCache.keys);
+        });
+    });
+}
+
+/**
+ * Custom logger to prevent circular reference in JSON.parse(obj)
+ */
+function log(msg, obj) {
+    console.log('\n');
+    if (obj) {
+        try {
+            console.log(msg + JSON.stringify(obj));
+        } catch (err) {
+            var simpleObject = {};
+            for (var prop in obj) {
+                if (!obj.hasOwnProperty(prop)) {
+                    continue;
+                }
+                if (typeof(obj[prop]) == 'object') {
+                    continue;
+                }
+                if (typeof(obj[prop]) == 'function') {
+                    continue;
+                }
+                simpleObject[prop] = obj[prop];
+            }
+            console.log('circular-' + msg + JSON.stringify(simpleObject)); // returns cleaned up JSON
+        }
+    } else {
+        console.log(msg);
+    }
+}
