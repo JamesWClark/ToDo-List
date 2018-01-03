844ea1f4c3b09f5d1b003c185bafcbddae8c4ec3
J.W. Clark <jwclark@rockhursths.edu>
2018-01-03 09:51:47 -0600

    Add wake detect web worker

--- client/client.js
+++ client/client.js
@@ -2,27 +2,54 @@
 // OR   : http://stackoverflow.com/questions/32150845/how-to-refresh-expired-google-sign-in-logins?rq=1
 // READ : http://stackoverflow.com/questions/3105296/if-rest-applications-are-supposed-to-be-stateless-how-do-you-manage-sessions
 // READ : http://www.cloudidentity.com/blog/2014/03/03/principles-of-token-validation/
+// TODO : https://jsfiddle.net/JamesWClark/8p8subj9/
+// CLIENT API: https://developers.google.com/identity/sign-in/web/reference#gapiauth2authresponse
 
-// give an error message on pseudo loopback interface
-if (window.location.hostname === '127.0.0.1') {
-    $('#app').html('Google Web Login requires `localhost` and not `127.0.0.1`. Try <a href="http://localhost:1898/client">here</a> instead.');
-    $('#app').show();
-}
+// CRITICAL TODO: https://stackoverflow.com/a/37580494/1161948
 
-// prepends the url of node.js server to any path
-function route(path) {
-    return 'http://localhost:3000' + path;
-}
 
 var profile; // google user profile
 var authResponse; // google user auth response
 
+function start() {
+    gapi.load('auth2', function() {
+        auth2 = gapi.auth2.init({
+            client_id : '570807057244-dgdihgn2co89soo39sb6td3o2j0m6f5p.apps.googleusercontent.com',
+            // Scopes to request in addition to 'profile' and 'email'
+            //scope: 'additional_scope'
+        })
+    })
+}
+
+//https://developers.google.com/identity/sign-in/web/server-side-flow
+function signInCallback(authResult) {
+    if(authResult['code']) {
+        
+        $('#signinButton').attr('style', 'display: none');
+        
+        console.log('authResult["code"] = ', authResult['code']);
+    } else {
+        console.log('signInCallback error');
+    }
+}
+
+$('#signinButton').click(function() {
+    auth2.grantOfflineAccess().then(signInCallback);
+});
+
 function onSignIn(googleUser) {
+    
+    // true = include access_token, see: https://stackoverflow.com/a/44773920/1161948
+    authResponse = googleUser.getAuthResponse(true); 
     profile = googleUser.getBasicProfile();
-    authResponse = googleUser.getAuthResponse();
+    
+    console.log('googleUser = ', googleUser);
+    console.log('profile = ', profile);
+    console.log('authResponse = ', authResponse);
 
     var login = {
         id: profile.getId(),
+        accessToken: authResponse.access_token,
         name: profile.getName(),
         givenName: profile.getGivenName(),
         familyName: profile.getFamilyName(),
@@ -31,7 +58,12 @@ function onSignIn(googleUser) {
         hostedDomain: googleUser.getHostedDomain()
     };
 
-    http.post('/login', login);
+    http.post('/login', login, function(data, statusText, jqXHR) {
+        if(jqXHR.status === 201) {
+            console.log('login ok ' + data);
+            // create a persistent access token cookie
+        }
+    });
     
     http.get('/tasks', function(data, statusText, jqXHR) {
         if(jqXHR.status === 200) {
@@ -67,6 +99,11 @@ function disconnect() {
 }
 
 var http = {};
+
+// prepends the url of node.js server to any path
+function route(path) {
+    return 'http://localhost:3000' + path;
+}
 
 http.post = function(url, json, success, error) {
     $.ajax({
@@ -177,3 +214,18 @@ $('#app').on('click', '.well .glyphicon-trash', function() {
         }
     });
 });
+
+// give an error message on pseudo loopback interface
+if (window.location.hostname === '127.0.0.1') {
+    $('#app').html('Google Web Login requires `localhost` and not `127.0.0.1`. Try <a href="http://localhost:1898/client">here</a> instead.');
+    $('#app').show();
+}
+
+// detect awake from computer sleep, used to refresh auth tokens
+var wakeWorker = new Worker('workers/detect-wake.js');
+wakeWorker.onmessage = function (ev) {
+  if (ev && ev.data === 'wakeup') {
+     console.log('awake');
+      // TODO: refresh auth token if it's expired
+  }
+}
